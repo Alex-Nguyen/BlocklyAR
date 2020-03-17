dd6b75052d4333b10ff6d95a5525712ffd2baa3d
Vinh T. Nguyen <vinhnt@tnu.edu.vn>
2020-03-17 11:13:52 -0500

    update Mar 17

--- assets/js/aframe_blocks.js
+++ assets/js/aframe_blocks.js
@@ -12,6 +12,7 @@ Blockly.defineBlocksWithJsonArray([
                 name: "content"
             }
         ],
+        previousStatement: "null",
         "colour": 355,
         "tooltip": "",
         "helpUrl": ""
@@ -20,10 +21,10 @@ Blockly.defineBlocksWithJsonArray([
         "type": "marker",
         "message0": "marker %1 %2 %3",
         "args0": [
-            {
-                type: "field_input",
-                name: "id",
-                text: "marker"
+            {    // Beginning of the field variable dropdown
+                "type": "field_variable",
+                "name": "VAR",    // Static name of the field
+                "variable": "item"    // Given at runtime
             },
             {
                 type: "input_value",
@@ -131,8 +132,13 @@ Blockly.defineBlocksWithJsonArray([
     },
     {
         type: "gltf",
-        message0: "GLTF position %1 rotation %2 source %3 animation %4",
+        message0: "GLTF id %1 position %2 rotation %3 source %4 animation %5",
         args0: [
+            {    // Beginning of the field variable dropdown
+                "type": "field_variable",
+                "name": "VAR",    // Static name of the field
+                "variable": "item"    // Given at runtime
+            },
             {
                 type: "input_value",
                 name: "position_vector3",
@@ -161,25 +167,25 @@ Blockly.defineBlocksWithJsonArray([
         colour: 65
     },
     {
-        type:"animation-mixer",
-        message0:"Animation %1 %2",
-        args0:[
+        type: "animation-mixer",
+        message0: "Animation %1 %2",
+        args0: [
             {
-                type:"input_dummy"
-            },{
-                type:"input_statement",
-                name:"NAME"
+                type: "input_dummy"
+            }, {
+                type: "input_statement",
+                name: "NAME"
             }
         ],
-        output:"attribute",
-        colour:90
+        output: "attribute",
+        colour: 90
     },
     {
-        type:"clip",
-        message0:"Clip %1",
-        args0:[
+        type: "clip",
+        message0: "Clip %1",
+        args0: [
             {
-                type:"field_dropdown",
+                type: "field_dropdown",
                 name: "clips",
                 options: [
                     ["animation_0", 'animation_0'],
@@ -187,15 +193,15 @@ Blockly.defineBlocksWithJsonArray([
                 ]
             }
         ],
-        previousStatement:"null",
-        nextStatement:"null"
+        previousStatement: "null",
+        nextStatement: "null"
     },
     {
-        type:"loop",
-        message0:"Loop %1",
-        args0:[
+        type: "loop",
+        message0: "Loop %1",
+        args0: [
             {
-                type:"field_dropdown",
+                type: "field_dropdown",
                 name: "loops",
                 options: [
                     ["Once", 'once'],
@@ -204,7 +210,263 @@ Blockly.defineBlocksWithJsonArray([
                 ]
             }
         ],
-        previousStatement:"null",
+        previousStatement: "null",
+        nextStatement: "null"
+    },
+    {
+        "type": "nav_bar",
+        "message0": "Navigation %1 %2",
+        "args0": [
+            {
+                type: "input_dummy"
+            },
+            {
+                type: "input_statement",
+                name: "content"
+            }
+        ],
+        "colour": 355,
+        nextStatement: "null",
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "register_component",
+        "message0": "Component %1 %2",
+        "args0": [
+            {    // Beginning of the field variable dropdown
+                "type": "field_variable",
+                "name": "VAR",    // Static name of the field
+                "variable": "item"    // Given at runtime
+            },
+            {
+                type: "input_statement",
+                name: "content"
+            }
+        ],
+        previousStatement: "null",
+        "colour": 355,
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "script",
+        "message0": "Script %1 %2",
+        "args0": [
+            {
+                type: "input_dummy"
+            },
+            {
+                type: "input_statement",
+                name: "content"
+            }
+        ],
+        previousStatement: "null",
+        "colour": 355,
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        type:"component_init",
+        message0:"Init %1",
+        args0:[
+            {
+                type:"input_statement",
+                name:"content"
+            }
+        ],
+        previousStatement: "null",
         nextStatement:"null"
-    }
+    },
+    {
+        type:"component_init_function",
+        message0:"Set Button %1 on click to Behavior %2",
+        args0:[
+            {    // Beginning of the field variable dropdown
+                "type": "field_variable",
+                "name": "VAR",    // Static name of the field
+                "variable": "item"    // Given at runtime
+            },
+            {
+                type:"input_value",
+                name:"content"
+            }
+        ],
+        previousStatement: "null",
+        nextStatement:"null"
+    },
+    {
+        type:"component_tick",
+        message0:"Ticks %1",
+        args0:[
+            {
+                type:"input_statement",
+                name:"content"
+            }
+        ],
+        previousStatement: "null",
+        nextStatement:"null"
+    },
+    {
+        type:"button",
+        message0:"Button ID %1 NAME %2",
+        args0:[
+            {    // Beginning of the field variable dropdown
+                "type": "field_variable",
+                "name": "VAR",    // Static name of the field
+                "variable": "item"    // Given at runtime
+            },
+            {
+                type:"field_input",
+                name:"NAME",
+                text: "Button 1",
+            }
+        ],
+        previousStatement: "null",
+        nextStatement:"null",
+        "colour": 355,
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        type: "behavior",
+        message0: "Behavior %1 %2",
+        args0: [
+            {
+                type: "input_dummy"
+            }, {
+                type: "input_statement",
+                name: "NAME"
+            }
+        ],
+        output: "null",
+        colour: 90
+    },
+    {
+        type: "print",
+        message0: "Alert %1 %2",
+        args0: [
+            {
+                type: "input_dummy"
+            },
+            {
+                type:"field_input",
+                name:"NAME",
+                text: "Hello world",
+            }
+        ],
+        previousStatement: "null",
+        nextStatement:"null",
+        colour: 90
+    },
+    {
+        type: "move_object",
+        message0: "Move Object %1 from %2 to %3",
+        args0: [
+            {    // Beginning of the field variable dropdown
+                "type": "field_variable",
+                "name": "VAR",    // Static name of the field
+                "variable": "item"    // Given at runtime
+            },
+            {    // Beginning of the field variable dropdown
+                "type": "field_variable",
+                "name": "START",    // Static name of the field
+                "variable": "item"    // Given at runtime
+            },
+            {    // Beginning of the field variable dropdown
+                "type": "field_variable",
+                "name": "END",    // Static name of the field
+                "variable": "item"    // Given at runtime
+            },
+        ],
+        previousStatement: "null",
+        nextStatement:"null",
+        colour: 90
+    },
+    {
+        type: "set_object_animation",
+        message0: "Set animation of %1 to %2",
+        args0: [
+            {    // Beginning of the field variable dropdown
+                "type": "field_variable",
+                "name": "VAR",    // Static name of the field
+                "variable": "item"    // Given at runtime
+            },
+            {    // Beginning of the field variable dropdown
+                "type": "field_variable",
+                "name": "ANIMATION",    // Static name of the field
+                "variable": "item"    // Given at runtime
+            }
+        ],
+        previousStatement: "null",
+        nextStatement:"null",
+        colour: 90
+    },
+    {
+        type: "move_object_finish",
+        message0: "When Object %1 finish moving do %2",
+        args0: [
+            {    // Beginning of the field variable dropdown
+                "type": "field_variable",
+                "name": "VAR",    // Static name of the field
+                "variable": "item"    // Given at runtime
+            },
+            {    // Beginning of the field variable dropdown
+                "type": "input_statement",
+                "name": "content",    // Static name of the field
+            }
+        ],
+        previousStatement: "null",
+        nextStatement:"null",
+        colour: 90
+    },
+
+    {
+        type: "trigger_behavior",
+        message0: "Trigger next behavior %1",
+        args0: [
+            {    // Beginning of the field variable dropdown
+                "type": "field_variable",
+                "name": "VAR",    // Static name of the field
+                "variable": "item"    // Given at runtime
+            }
+        ],
+        previousStatement: "null",
+        nextStatement:"null",
+        colour: 90
+    },
+    {
+        type: "play_audio",
+        message0: "Play Audio %1",
+        args0: [
+            {
+                type: "field_dropdown",
+                name: "audio",
+                options: [
+                    ["audio1", 'assets/audio/message01.mp3'],
+                    ["audio2", 'assets/audio/message02.mp3'],
+                ]
+            }
+        ],
+        previousStatement: "null",
+        nextStatement:"null",
+        colour: 90
+    },
+    {
+        type: "wait_second",
+        message0: "Wait %1 seconds THEN %2",
+        args0: [
+            {    // Beginning of the field variable dropdown
+                "type": "field_number",
+                "name": "NAME",    // Static name of the field
+            },
+            {    // Beginning of the field variable dropdown
+                "type": "input_statement",
+                "name": "content",    // Static name of the field
+            }
+        ],
+        previousStatement: "null",
+        nextStatement:"null",
+        colour: 90
+    },
 ]);
--- assets/js/aframe_generators.js
+++ assets/js/aframe_generators.js
@@ -1,9 +1,9 @@
 var HtmlGenerator = new Blockly.Generator("HTML");
-HtmlGenerator.init = function (workspace) {}
+HtmlGenerator.init = function (workspace) {
+}
 HtmlGenerator.finish = function (code) {
     return code;
 };
-
 HtmlGenerator.scrub_ = function (block, code) {
     let nextBlock = block.nextConnection && block.nextConnection.targetBlock();
     let nextCode = HtmlGenerator.blockToCode(nextBlock);
@@ -13,12 +13,21 @@ HtmlGenerator.scrub_ = function (block, code) {
 
 HtmlGenerator['ar_scene'] = function (block) {
       let statement_content = HtmlGenerator.statementToCode(block,"content");
-      return `\n${statement_content}\n`;
+      return `<a-scene manager arjs embedded>\n${statement_content}\n<a-entity camera></a-entity>\n</a-scene>`;
+};
+
+HtmlGenerator['nav_bar'] = function (block) {
+    let statement_content = HtmlGenerator.statementToCode(block,"content");
+    return `<div class="fixed-top mt-4">\n <ul class="nav nav-pills nav-justified">${statement_content}\n</ul>\n</div>\n`;
 };
 
 HtmlGenerator['marker'] = function (block) {
+    Blockly.JavaScript.init(Blockly.mainWorkspace)
+    let id = Blockly.JavaScript.variableDB_.getName(
+        block.getFieldValue('VAR'), Blockly.Variables.NAME_TYPE);
     let value_name = HtmlGenerator.valueToCode(block, "NAME", Blockly.JavaScript.ORDER_ATOMIC);
-    let id = block.getFieldValue("id");
+    Blockly.JavaScript.addReservedWords('code');
+
     let statement_content = HtmlGenerator.statementToCode(block, "content");
     return `<a-marker project='visAR' id='${id}' ${value_name}>\n${statement_content}\n</a-marker>\n`;
 };
@@ -47,7 +56,7 @@ HtmlGenerator['cube'] = function (block) {
     if(pos !=="") pos  = `position = '${pos}'`;
     if(rot !=="") rot  = `rotation = '${rot}'`;
 
-    return `<a-box project='visAR' ${pos} ${rot}></a-box>`;
+    return `<a-box material="opacity:0.1" project='visAR' ${pos} ${rot}></a-box>`;
 };
 HtmlGenerator['animation-mixer'] = function (block) {
     let pos = HtmlGenerator.valueToCode(block,'position_vector3', Blockly.JavaScript.ORDER_ATOMIC);
@@ -67,8 +76,10 @@ HtmlGenerator['gltf'] = function (block) {
     if(ani !=="") ani = `${ani}`;
     if(pos !=="") pos  = `position = '${pos}'`;
     if(rot !=="") rot  = `rotation = '${rot}'`;
-
-    return `<a-gltf-model project='visAR' ${pos} ${rot}${src}${ani}></a-gltf-model>`;
+    Blockly.JavaScript.init(Blockly.mainWorkspace)
+    let id = Blockly.JavaScript.variableDB_.getName(
+        block.getFieldValue('VAR'), Blockly.Variables.NAME_TYPE);
+    return `<a-gltf-model id="${id}" project='visAR' ${pos} ${rot}${src}${ani}></a-gltf-model>`;
 }
 HtmlGenerator['vector3'] = function (block) {
     let x = block.getFieldValue('x');
@@ -88,4 +99,116 @@ HtmlGenerator['animation-mixer'] = function (block) {
 HtmlGenerator['clip'] = function (block) {
     let lval = block.getFieldValue('clips');
    return `clip: ${lval};`;
+}
+//
+HtmlGenerator['register_component'] = function (block) {
+    let statement_content = HtmlGenerator.statementToCode(block,"content");
+    Blockly.JavaScript.init(Blockly.mainWorkspace)
+    let component_name = Blockly.JavaScript.variableDB_.getName(
+        block.getFieldValue('VAR'), Blockly.Variables.NAME_TYPE);
+    return `AFRAME.registerComponent('${component_name}', {\n${statement_content}\n});`;
+};
+
+HtmlGenerator['script'] = function (block) {
+    let statement_content = HtmlGenerator.statementToCode(block,"content");
+    return `<script>\n${statement_content}\n</script>`;
+};
+HtmlGenerator['component_init'] = function (block) {
+    let statement_content = HtmlGenerator.statementToCode(block,"content");
+
+    return `init: function(){\n${statement_content}\n}\n`
+}
+HtmlGenerator['component_init_function'] = function (block) {
+    let statement_content = HtmlGenerator.statementToCode(block,"content");
+    Blockly.JavaScript.init(Blockly.mainWorkspace)
+    let component_name = Blockly.JavaScript.variableDB_.getName(
+        block.getFieldValue('VAR'), Blockly.Variables.NAME_TYPE);
+    return `$("#${component_name}").click(${statement_content});\n`
+}
+
+HtmlGenerator['component_tick'] = function (block) {
+    return `tick: function(time, deltaTime){}\n`
+}
+HtmlGenerator['button'] = function (block) {
+    Blockly.JavaScript.init(Blockly.mainWorkspace)
+    let variable_name = Blockly.JavaScript.variableDB_.getName(
+        block.getFieldValue('VAR'), Blockly.Variables.NAME_TYPE);
+    let btnName = block.getFieldValue("NAME");
+
+    return `\n<li class="nav-item"><button type="button" id='${variable_name}' class="btn btn-dark stage">${btnName}</button></li>\n`;
+};
+
+HtmlGenerator['behavior'] = function (block) {
+    let statement_content = HtmlGenerator.statementToCode(block, "NAME");
+    return `function(){${statement_content}}`;
+}
+HtmlGenerator['print'] = function (block) {
+    let value = block.getFieldValue('NAME');
+    return `\n window.alert('${value}');\n`;
+}
+HtmlGenerator['move_object'] = function (block) {
+    Blockly.JavaScript.init(Blockly.mainWorkspace)
+    let value = block.getFieldValue('NAME');
+    let var_object = Blockly.JavaScript.variableDB_.getName(
+        block.getFieldValue('VAR'), Blockly.Variables.NAME_TYPE);
+    let var_start = Blockly.JavaScript.variableDB_.getName(
+        block.getFieldValue('START'), Blockly.Variables.NAME_TYPE);
+    let var_end = Blockly.JavaScript.variableDB_.getName(
+        block.getFieldValue('END'), Blockly.Variables.NAME_TYPE);
+    let code =`\n let scene${var_object} = document.querySelector('a-scene');\n`;
+    code += `\n let pos1 = document.querySelector('#${var_start}').object3D.position;\n`;
+    code += `\n let pos2 = document.querySelector('#${var_end}').object3D.position;\n`;
+    code += `\n let $obj${var_object} = document.querySelector('#${var_object}')\n`;
+    code += `\n $obj${var_object}.parentNode.removeChild($obj${var_object})\n`;
+    code += `\n scene${var_object}.appendChild($obj${var_object}.cloneNode());\n`;
+    code += `\n $($obj${var_object}).attr("position",[pos1.x, pos1.y, pos1.z].join(" "))\n`;
+    code += `\n let curve = document.createElement('a-curve');\n`;
+    code += `\n $("#${var_start}${var_end}").remove(); \n`;
+    code += `\n curve.setAttribute('id', '${var_start}${var_end}');\n`;
+    code += `\n let curvePoint1 = document.createElement('a-curve-point');\n`;
+    code += `\n let curvePoint2 = document.createElement('a-curve-point');\n`;
+    code += `\n curvePoint1.setAttribute('position',[pos1.x, pos1.y, pos1.z].join(" "));\n`;
+    code += `\n curvePoint2.setAttribute('position', [pos2.x, pos2.y, pos2.z].join(" "));\n`;
+    code += `\n curve.appendChild(curvePoint1);\n`;
+    code += `\n curve.appendChild(curvePoint2);\n`;
+    code += `\n scene${var_object}.appendChild(curve);\n`;
+    code += `\n $("#${var_object}").attr("alongpath","curve: #${var_start}${var_end}; dur: 3000; rotate: true; delay: 2000")\n`;
+    return code;
+}
+
+HtmlGenerator['set_object_animation'] = function (block) {
+    Blockly.JavaScript.init(Blockly.mainWorkspace);
+    let var_object = Blockly.JavaScript.variableDB_.getName(
+        block.getFieldValue('VAR'), Blockly.Variables.NAME_TYPE);
+    let var_animation = Blockly.JavaScript.variableDB_.getName(
+        block.getFieldValue('ANIMATION'), Blockly.Variables.NAME_TYPE);
+
+    return `\n  $("#${var_object}").attr("animation-mixer","clip: ${var_animation}");\n`;
+}
+
+HtmlGenerator['move_object_finish'] = function (block) {
+    Blockly.JavaScript.init(Blockly.mainWorkspace)
+    let var_object = Blockly.JavaScript.variableDB_.getName(
+        block.getFieldValue('VAR'), Blockly.Variables.NAME_TYPE);
+    let statement_content = HtmlGenerator.statementToCode(block, "content");
+
+    return `$("#${var_object}").off("movingended","**")\n$("#${var_object}").on('movingended', function(){${statement_content}\n})`;
+}
+
+HtmlGenerator['trigger_behavior'] = function (block) {
+    Blockly.JavaScript.init(Blockly.mainWorkspace)
+    let var_object = Blockly.JavaScript.variableDB_.getName(
+        block.getFieldValue('VAR'), Blockly.Variables.NAME_TYPE);
+    return `\n$("#${var_object}").trigger('click');\n`;
+}
+
+HtmlGenerator['play_audio'] = function (block) {
+    let src = block.getFieldValue("audio")
+    return `\n let audio = new Audio("${src}");\n audio.play()`;
+}
+HtmlGenerator['wait_second'] = function (block) {
+    let statement_content = HtmlGenerator.statementToCode(block, "content");
+
+    let sec = block.getFieldValue("NAME")
+    return `\n setTimeout(function(){${statement_content}},${sec*1000})`;
 }
